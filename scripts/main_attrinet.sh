94a8b48ff6a21031b73926f92bfdf73e1c2d584a
sun22 <susu.sun@uni-tuebingen.de>
2023-11-10 21:00:14 +0100

    combine bbox + pseudo guidance of attrinet into one file

--- scripts/main_attrinet.sh
+++ scripts/main_attrinet.sh
@@ -10,7 +10,7 @@
 #SBATCH --error=/mnt/qb/work/baumgartner/sun22/logs/hostname_%j.err   # File to which STDERR will be written
 #SBATCH --mail-type=FAIL           # Type of email notification- BEGIN,END,FAIL,ALL
 #SBATCH --mail-user=<susu.sun@uni-tuebingen.de>  # Email to which notifications will be sent
-#SBATCH --array=25, 30, 34, 40   #
+#SBATCH --array=25,30,34,40   #
 
 # print info about current job
 scontrol show job $SLURM_JOB_ID 
