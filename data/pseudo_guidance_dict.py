f3576ae61a10ca32f6caa0353db5796d91a8b995
sun22 <susu.sun@uni-tuebingen.de>
2023-11-10 20:14:51 +0100

    combine bbox + pseudo guidance of attrinet into one file

--- data/pseudo_guidance_dict.py
+++ data/pseudo_guidance_dict.py
@@ -2,12 +2,12 @@ import os
 
 cwd = os.getcwd()
 pseudo_mask_dict = {
-    "chexpert": os.path.join(cwd, "/data/pseudo_masks_chexpert.json"),
+    "chexpert": os.path.join(cwd, "data/pseudo_masks_chexpert.json"),
     "nih_chestxray": os.path.join(cwd,"data/pseudo_masks_nih.json"),
 }
 
 pseudo_bbox_dict = {
-    "chexpert": os.path.join(cwd, "/data/pseudo_bboxs_chexpert.json"),
-    "nih_chestxray": os.path.join(cwd,"/data/pseudo_bboxs_nih.json"),
+    "chexpert": os.path.join(cwd, "data/pseudo_bboxs_chexpert.json"),
+    "nih_chestxray": os.path.join(cwd,"data/pseudo_bboxs_nih.json"),
 }
 
