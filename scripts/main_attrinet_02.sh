e00bacd66d8184aeb201a0d3988c0c99b5e5e135
sun22 <susu.sun@uni-tuebingen.de>
2023-11-28 18:04:17 +0100

    attrinet with guidance vindr

--- scripts/main_attrinet_02.sh
+++ scripts/main_attrinet_02.sh
@@ -48,7 +48,13 @@ echo "-------- PYTHON OUTPUT ----------"
 # python3 main.py --manual_seed 21
 # python3 main.py --manual_seed 4294438
 
-python3 main.py --debug "False" --lambda_localizationloss 25
+# python3 main_attrinet.py --debug "False" --dataset_idx ${SLURM_ARRAY_TASK_ID} --use_wandb "False" # train attrinet on gray and color glocuma images
+# python3 main_attrinet.py --debug "False" --dataset_idx ${SLURM_ARRAY_TASK_ID} --lambda_3 0 --lambda_centerloss 0 --lambda_localizationloss 0 --use_wandb "False" # train attrinet on gray and color glocuma images wihtout classifiers loss
+
+# python3 main_attrinet.py --debug "False" --dataset_idx ${SLURM_ARRAY_TASK_ID} --use_wandb "False"
+
+# python3 main_attrinet.py --debug "False" --guidance_mode 'bbox' --dataset_idx 6 --lambda_localizationloss ${SLURM_ARRAY_TASK_ID} --use_wandb "False"
+python3 main_attrinet.py --guidance_freq 0.2
 
 echo "---------------------------------"
 
