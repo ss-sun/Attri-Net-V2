472aae1f0703c9dead5418f59eb2461d59c554a4
sun22 <susu.sun@uni-tuebingen.de>
2023-12-12 15:29:48 +0100

    attrinet guidance on global shortcut

--- vis/nih_pseudobbox.py
+++ vis/nih_pseudobbox.py
@@ -21,7 +21,40 @@ def get_bbox_list(src_annotation, disease_name):
 
 
 
-def draw_bbox(src_img_path, bbox_list, out_dir):
+
+def draw_gt_bbox(src_img_path, src_annotation, out_dir):
+    df = pd.read_csv(src_annotation)
+    file_name = os.path.basename(src_img_path)
+    img = Image.open(src_img_path)
+    rgb_img = img.convert('RGB')
+    for index, row in df.iterrows():
+        if row["Finding Label"] == "Cardiomegaly" and row["Image Index"] == file_name:
+            x = row["Bbox [x"]
+            y = row["y"]
+            w = row["w"]
+            h = row["h]"]
+            bbox = [x, y, w, h]
+
+    rgb_mask = np.zeros((rgb_img.size[0], rgb_img.size[1], 3), dtype=np.uint8)
+    mask_img = Image.fromarray(rgb_mask).convert('RGB')
+
+    draw = ImageDraw.Draw(mask_img)
+    outline_color = (0, 255, 0)
+    outline_width = 2
+
+    draw.rectangle([bbox[0], bbox[1], bbox[0]+bbox[2], bbox[1]+bbox[3]], outline=outline_color, width=outline_width)
+    del draw
+
+    mask_img.putalpha(50)
+    rgb_img.paste(mask_img, (0, 0), mask_img)
+
+    file_name = os.path.basename(src_img_path)
+    path = os.path.join(out_dir, "gt_bbox_" + file_name)
+    rgb_img.save(path)
+
+
+
+def draw_bbox_list(src_img_path, bbox_list, out_dir):
 
     img = Image.open(src_img_path)
     rgb_img = img.convert('RGB')
@@ -111,6 +144,9 @@ def draw_speudo_bbox(src_img_path, bbox_list, out_dir):
     rgb_img.save(path)
 
 
+
+
+
 if __name__ == "__main__":
 
     src_img = "00007551_020.png" # nih bbox example with cardiomegaly positive
@@ -118,6 +154,7 @@ if __name__ == "__main__":
     out_dir = "/home/susu/Documents/MIDL_journal/journal_figures/speudo_bbox"
     scaled_bbox_annotation = "/mnt/qb/work/baumgartner/sun22/data/NIH_BB/NIHChestX-rays/BBox_valid_df_scaled.csv"
     bbox_list = get_bbox_list(scaled_bbox_annotation, disease_name="Cardiomegaly")
-    draw_bbox(os.path.join(src_dir,src_img), bbox_list, out_dir)
+    draw_gt_bbox(os.path.join(src_dir, src_img), scaled_bbox_annotation, out_dir)
+    draw_bbox_list(os.path.join(src_dir,src_img), bbox_list, out_dir)
     draw_speudo_masks(os.path.join(src_dir, src_img), bbox_list, out_dir)
     draw_speudo_bbox(os.path.join(src_dir, src_img), bbox_list, out_dir)
