24b27c87d71adedb104d9d61eb01ffc7ede386cc
sun22 <susu.sun@uni-tuebingen.de>
2023-12-12 08:30:42 +0100

    attrinet guidance on global

--- scripts/attrinet_guid_contain_chexpert.sh
+++ scripts/attrinet_guid_contain_chexpert.sh
@@ -38,7 +38,7 @@ conda activate tt_interaction
 # Run our code
 echo "-------- PYTHON OUTPUT ----------"
 
-python3 main_attrinet.py --debug "False" --guidance_mode 'guidance_shortcut' --guidance_freq 0 --dataset "contaminated_chexpert" --lambda_localizationloss 30 --num_iters 100000
+python3 main_attrinet.py --debug "False" --guidance_mode 'guidance_shortcut' --guidance_freq 0 --dataset "contaminated_chexpert" --lambda_localizationloss 100 --num_iters 100000
 
 echo "---------------------------------"
 
