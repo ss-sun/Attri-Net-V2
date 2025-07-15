9d6941c11401e5413d53d240e9e904d9f67c8dc2
sun22 <susu.sun@uni-tuebingen.de>
2025-07-15 15:00:34 +0200

    t test

--- evaluations/eval_class_sensitivity.py
+++ evaluations/eval_class_sensitivity.py
@@ -356,10 +356,11 @@ if __name__ == "__main__":
     # set the variables here:
 
     evaluated_models = t_test_models
+    file_name = str(datetime.datetime.now())[:-7] + "eval_class_sensitivity_" + "t_test_models" + ".json"
 
 
-    evaluated_models = guided_attrinet_models
-    file_name = str(datetime.datetime.now())[:-7] + "eval_class_sensitivity_" + "guided_attrinet_models" + ".json"
+    # evaluated_models = guided_attrinet_models
+    # file_name = str(datetime.datetime.now())[:-7] + "eval_class_sensitivity_" + "guided_attrinet_models" + ".json"
 
     # evaluated_models = attrinet_models
     # file_name = str(datetime.datetime.now())[:-7] + "eval_class_sensitivity_" + "attrinet_models" + ".json"
